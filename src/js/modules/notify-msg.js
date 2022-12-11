a5a3f47109b611908e03d5eb68070d143caf9047
Valov Ivan <106810267+Pechenkooo3@users.noreply.github.com>
2022-12-11 20:40:11 +0200

    1

--- src/js/modules/notify-msg.js
+++ src/js/modules/notify-msg.js
@@ -25,21 +25,6 @@ export const notifySuccessMessage = totalHits => {
   });
 };
 
-export const notifyInfoMessage = () => {
-  return Notify.info(
-    "We're sorry, but you've reached the end of search results.",
-    {
-      opacity: 1,
-      position: 'center-center',
-      timeout: 500,
-      background: '#0c09db',
-      backOverlay: true,
-      cssAnimationDuration: 1000,
-      backOverlayColor: 'rgba(0, 153, 255, 0.313)',
-      cssAnimationStyle: 'zoom',
-    }
-  );
-};
 
 export const notifyInfoSearchMessage = () => {
   return Notify.info('Please fill the search field!', {
